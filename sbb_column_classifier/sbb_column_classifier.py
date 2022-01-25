143da605f15155f0d0e53ad03b00b30bc4d750d6
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2022-01-25 17:32:52 +0100

    🧹 Clean up whitespace a little bit

--- sbb_column_classifier/sbb_column_classifier.py
+++ sbb_column_classifier/sbb_column_classifier.py
@@ -36,21 +36,21 @@ __doc__ = \
     """
     tool to extract table form data from alto xml data
     """
+
 class sbb_column_classifier:
     def __init__(self, image_dir, dir_models):
         self.image_dir = image_dir  # XXX This does not seem to be a directory as the name suggests, but a file
         self.kernel = np.ones((5, 5), np.uint8)
-        
+
         self.image = cv2.imread(self.image_dir)
         self.model_classifier_dir = dir_models + "/model_scale_classifier.h5"
         self.model_page_dir = dir_models + "/model_page_mixed_best.h5"
         #self.image_filename=Path(self.image_dir).name
         #print(self.image_filename,'self.image_filename_stem')
-            
+
     def resize_image(self, img_in, input_height, input_width):
         return cv2.resize(img_in, (input_width, input_height), interpolation=cv2.INTER_NEAREST)
 
-            
     def start_new_session_and_model(self, model_dir):
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
@@ -59,22 +59,19 @@ class sbb_column_classifier:
         model = load_model(model_dir, compile=False)
 
         return model, session
-    
+
     def do_prediction(self,patches,img,model,marginal_of_patch_percent=0.1):
-        
         img_height_model = model.layers[len(model.layers) - 1].output_shape[1]
         img_width_model = model.layers[len(model.layers) - 1].output_shape[2]
         n_classes = model.layers[len(model.layers) - 1].output_shape[3]
-        
-
 
         if patches:
             if img.shape[0]<img_height_model:
                 img=self.resize_image(img,img_height_model,img.shape[1])
-                
+
             if img.shape[1]<img_width_model:
                 img=self.resize_image(img,img.shape[0],img_width_model)
-            
+
             #print(img_height_model,img_width_model)
             #margin = int(0.2 * img_width_model)
             margin = int(marginal_of_patch_percent * img_height_model)
@@ -86,9 +83,9 @@ class sbb_column_classifier:
             img = img / float(255.0)
             #print(sys.getsizeof(img))
             #print(np.max(img))
-            
+
             img=img.astype(np.float16)
-            
+
             #print(sys.getsizeof(img))
 
             img_h = img.shape[0]
@@ -132,8 +129,8 @@ class sbb_column_classifier:
                     if index_y_u > img_h:
                         index_y_u = img_h
                         index_y_d = img_h - img_height_model
-                        
-                    
+
+
 
                     img_patch = img[index_y_d:index_y_u, index_x_d:index_x_u, :]
 
@@ -151,7 +148,7 @@ class sbb_column_classifier:
                         mask_true[index_y_d + 0:index_y_u - margin, index_x_d + 0:index_x_u - margin] = seg
                         prediction_true[index_y_d + 0:index_y_u - margin, index_x_d + 0:index_x_u - margin,
                         :] = seg_color
-                        
+
                     elif i==nxf-1 and j==nyf-1:
                         seg_color = seg_color[margin:seg_color.shape[0] - 0, margin:seg_color.shape[1] - 0, :]
                         seg = seg[margin:seg.shape[0] - 0, margin:seg.shape[1] - 0]
@@ -159,7 +156,7 @@ class sbb_column_classifier:
                         mask_true[index_y_d + margin:index_y_u - 0, index_x_d + margin:index_x_u - 0] = seg
                         prediction_true[index_y_d + margin:index_y_u - 0, index_x_d + margin:index_x_u - 0,
                         :] = seg_color
-                        
+
                     elif i==0 and j==nyf-1:
                         seg_color = seg_color[margin:seg_color.shape[0] - 0, 0:seg_color.shape[1] - margin, :]
                         seg = seg[margin:seg.shape[0] - 0, 0:seg.shape[1] - margin]
@@ -167,7 +164,7 @@ class sbb_column_classifier:
                         mask_true[index_y_d + margin:index_y_u - 0, index_x_d + 0:index_x_u - margin] = seg
                         prediction_true[index_y_d + margin:index_y_u - 0, index_x_d + 0:index_x_u - margin,
                         :] = seg_color
-                        
+
                     elif i==nxf-1 and j==0:
                         seg_color = seg_color[0:seg_color.shape[0] - margin, margin:seg_color.shape[1] - 0, :]
                         seg = seg[0:seg.shape[0] - margin, margin:seg.shape[1] - 0]
@@ -175,7 +172,7 @@ class sbb_column_classifier:
                         mask_true[index_y_d + 0:index_y_u - margin, index_x_d + margin:index_x_u - 0] = seg
                         prediction_true[index_y_d + 0:index_y_u - margin, index_x_d + margin:index_x_u - 0,
                         :] = seg_color
-                        
+
                     elif i==0 and j!=0 and j!=nyf-1:
                         seg_color = seg_color[margin:seg_color.shape[0] - margin, 0:seg_color.shape[1] - margin, :]
                         seg = seg[margin:seg.shape[0] - margin, 0:seg.shape[1] - margin]
@@ -183,7 +180,7 @@ class sbb_column_classifier:
                         mask_true[index_y_d + margin:index_y_u - margin, index_x_d + 0:index_x_u - margin] = seg
                         prediction_true[index_y_d + margin:index_y_u - margin, index_x_d + 0:index_x_u - margin,
                         :] = seg_color
-                        
+
                     elif i==nxf-1 and j!=0 and j!=nyf-1:
                         seg_color = seg_color[margin:seg_color.shape[0] - margin, margin:seg_color.shape[1] - 0, :]
                         seg = seg[margin:seg.shape[0] - margin, margin:seg.shape[1] - 0]
@@ -191,7 +188,7 @@ class sbb_column_classifier:
                         mask_true[index_y_d + margin:index_y_u - margin, index_x_d + margin:index_x_u - 0] = seg
                         prediction_true[index_y_d + margin:index_y_u - margin, index_x_d + margin:index_x_u - 0,
                         :] = seg_color
-                        
+
                     elif i!=0 and i!=nxf-1 and j==0:
                         seg_color = seg_color[0:seg_color.shape[0] - margin, margin:seg_color.shape[1] - margin, :]
                         seg = seg[0:seg.shape[0] - margin, margin:seg.shape[1] - margin]
@@ -199,7 +196,7 @@ class sbb_column_classifier:
                         mask_true[index_y_d + 0:index_y_u - margin, index_x_d + margin:index_x_u - margin] = seg
                         prediction_true[index_y_d + 0:index_y_u - margin, index_x_d + margin:index_x_u - margin,
                         :] = seg_color
-                        
+
                     elif i!=0 and i!=nxf-1 and j==nyf-1:
                         seg_color = seg_color[margin:seg_color.shape[0] - 0, margin:seg_color.shape[1] - margin, :]
                         seg = seg[margin:seg.shape[0] - 0, margin:seg.shape[1] - margin]
@@ -222,7 +219,7 @@ class sbb_column_classifier:
             del seg_color
             del seg
             del img_patch
-                
+
         if not patches:
             img_h_page=img.shape[0]
             img_w_page=img.shape[1]
@@ -236,25 +233,25 @@ class sbb_column_classifier:
             seg_color =np.repeat(seg[:, :, np.newaxis], 3, axis=2)
             prediction_true = self.resize_image(seg_color, img_h_page, img_w_page)
             prediction_true = prediction_true.astype(np.uint8)
-            
-            
+
             del img
             del seg_color
             del label_p_pred
             del seg
+
         del model
         gc.collect()
-        
+
         return prediction_true
-    
+
     def crop_image_inside_box(self, box, img_org_copy):
         image_box = img_org_copy[box[1]:box[1] + box[3], box[0]:box[0] + box[2]]
         return image_box, [box[1], box[1] + box[3], box[0], box[0] + box[2]]
-    
+
     def extract_number_of_columns(self, image_page):
         patches=False
         model_classifier, session_classifier = self.start_new_session_and_model(self.model_classifier_dir)
-        
+
         img_in = image_page / 255.0
         img_in = cv2.resize(img_in, (448, 448), interpolation=cv2.INTER_NEAREST)
         img_in = img_in.reshape(1, 448, 448, 3)
@@ -267,7 +264,7 @@ class sbb_column_classifier:
         gc.collect()
 
         return num_col
-            
+
     def extract_page(self):
         patches=False
         model_page, session_page = self.start_new_session_and_model(self.model_page_dir)
@@ -275,9 +272,9 @@ class sbb_column_classifier:
         for ii in range(1):
             img = cv2.GaussianBlur(self.image, (5, 5), 0)
 
-        
+
         img_page_prediction=self.do_prediction(patches,img,model_page)
-        
+
         imgray = cv2.cvtColor(img_page_prediction, cv2.COLOR_BGR2GRAY)
         _, thresh = cv2.threshold(imgray, 0, 255, 0)
 
@@ -289,28 +286,28 @@ class sbb_column_classifier:
         cnt = contours[np.argmax(cnt_size)]
 
         x, y, w, h = cv2.boundingRect(cnt)
-        
+
 
         if x<=30:
             w=w+x
             x=0
         if (self.image.shape[1]-(x+w) )<=30:
             w=w+(self.image.shape[1]-(x+w) )
-        
+
         if y<=30:
             h=h+y
             y=0
         if (self.image.shape[0]-(y+h) )<=30:
             h=h+(self.image.shape[0]-(y+h) )
-            
-            
+
+
 
         box = [x, y, w, h]
 
         croped_page, page_coord = self.crop_image_inside_box(box, self.image)
-        
+
         self.cont_page=[]
-        self.cont_page.append( np.array( [ [ page_coord[2] , page_coord[0] ] , 
+        self.cont_page.append( np.array( [ [ page_coord[2] , page_coord[0] ] ,
                                                     [ page_coord[3] , page_coord[0] ] ,
                                                     [ page_coord[3] , page_coord[1] ] ,
                                                 [ page_coord[2] , page_coord[1] ]] ) )
@@ -325,8 +322,8 @@ class sbb_column_classifier:
 
         gc.collect()
         return croped_page, page_coord
-    def run(self):
 
+    def run(self):
         image_page,_=self.extract_page()
         number_of_columns =self.extract_number_of_columns(image_page)
         if number_of_columns==1:
