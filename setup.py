9d898c830aaf1332ebd53e4e847092d4dc16a45a
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2022-01-25 15:15:12 +0100

    ⚙️ Install via setup.py, not via custom shell script
    make.sh had a fixed path, does not install dependencies, does not handle
    virtualenvs, does not support pip install etc.
    
    → Use standard setup.py method to handle installation sanely (pip
    install .)

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+from io import open
+from setuptools import setup
 
-import setuptools
-from numpy.distutils.core import Extension, setup
 
-setup(name='sbb_column_classifier',version=1.0,packages=['sbb_column_classifier'])
+with open("requirements.txt") as fp:
+    install_requires = fp.read()
+
+setup(
+    name='sbb_column_classifier',
+    packages=['sbb_column_classifier'],
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "sbb_column_classifier=sbb_column_classifier.sbb_column_classifier:main"
+        ]
+    }
+)
